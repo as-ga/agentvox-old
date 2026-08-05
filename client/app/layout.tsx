01cffe3516099d71eb811d527d14e777fa002861
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-08-05 08:48:34 +0530

    add shadcn

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Agentvox",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={GeistSans.className}>{children}</body>
     </html>
   );
